0f086a6b9ac81447d184ad812154ec9ae9da5053
Apurbo Saha <92245030+apurbo-saha@users.noreply.github.com>
2022-09-01 10:58:01 +0600

    Create timer.py

---
+++ timer.py
@@ -0,0 +1,28 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+
+google = pyttsx3.init()
+listener = sr.Recognizer()
+
+
+
+google.say("how can i help you sir")
+google.runAndWait()
+
+
+try:
+   with sr.Microphone() as source:
+       print("listening...")
+       voice = listener.listen(source)
+       command = listener.recognize_google(voice)
+       print(command)
+
+   if "time" in command:
+     time = datetime.datetime.now().strftime("%I:%M %p")
+     print(time)
+     google.say("current time is " + time)
+     google.runAndWait()
+
+except:
+    pass
